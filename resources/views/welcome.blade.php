02582ac34298cb340bffce18a90dacb9e832680e
aziz <aqawass@yahoo.com>
2021-08-19 12:19:07 -0400

    02 route

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,8 +85,8 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="{{route('a',55)}}">a</a>
+                    <a href="{{route('b')}}">b</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://blog.laravel.com">Blog</a>
                     <a href="https://nova.laravel.com">Nova</a>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,28 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/test1', function () {
+    return 'Welcome...';
+});
+
+//route parameters
+
+//requir param
+Route::get('/test2/{id}', function ($id) {
+    return $id;
+});
+//optional param
+Route::get('/test3/{id?}', function ($id=1) {
+    return 'welcome 3' . $id;
+});
+Route::get('/test4/{id?}', function () {
+    return 'welcome 4';
+});
+
+//route name
+Route::get('/show-number/{id}', function ($id) {
+    return $id;
+}) -> name('a');
+Route::get('/show-string/{id?}', function () {
+    return 'welcome 6';
+}) -> name('b');
