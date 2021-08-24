937cea058e8a61aae101cf1d02735e1201b90a88
aziz <aqawass@yahoo.com>
2021-08-24 02:42:31 -0400

    20 expire users task & send daily e-mail using schedule task

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+
+use App\Console\Commands\expiration;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +15,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        expiration::class,
+
     ];
 
     /**
@@ -26,6 +29,8 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->command('user:expire')->everyMinute();
+        $schedule->command('notify:email')->everyMinute()->appendOutputTo(storage_path('logs/inspire.log'));;   //daily();
     }
 
     /**
