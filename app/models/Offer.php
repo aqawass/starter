c1cd1ba43e8bcc210e06d8e36e70c2ad6824c6ef
aziz <aqawass@yahoo.com>
2021-08-24 14:38:00 -0400

    Laravel Eloquent-ORM and model

---
+++ app/models/Offer.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Offer extends Model
+{
+    //protected $table = "myoffers";  in case table name in mysql is "myoffers"
+}
